b51a5bcbdf2e00bc59803788a3d9c52a48a15b88
Jacky Li <jackydli95@gmail.com>
2026-07-07 23:12:15 -0700

    Prepare v0.1.0a1: release workflow (PyPI trusted publishing)

--- src/quantbench/__init__.py
+++ src/quantbench/__init__.py
@@ -3,5 +3,5 @@
 from .bench import BenchResult, load_entrypoint, run_config
 from .configs import CONFIGS
 
-__version__ = "0.1.0.dev0"
+__version__ = "0.1.0a1"
 __all__ = ["BenchResult", "CONFIGS", "load_entrypoint", "run_config", "__version__"]
